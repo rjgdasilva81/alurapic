bddecbac8b83a81a7ce20dee96dc6d3252b0a713
Robson Galvao Silva <rjgdasilva@gmail.com>
2016-11-06 20:46:18 -0200

    Inclusão do Projeto

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,19 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+    $scope.fotos = [];
+    
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    })
+    .error(function(error){
+        console.log(erro);
+    });
+
+    /*var promise = $http.get('v1/fotos');
+    promise.then(function(retorno){
+        $scope.fotos = retorno.data;
+    }).catch(function(error){
+        console.log(error);
+    });*/
+
+});
