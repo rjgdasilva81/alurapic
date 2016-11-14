9d2e0fbba3e1184261e74feb6667ecaf8a1cf702
Robson Galvao Silva <rjgdasilva@gmail.com>
2016-11-14 02:34:49 -0200

    Novas aulas - Alura

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,5 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
     $scope.fotos = [];
+    $scope.filtro = '';
+    $scope.mensagem = '';
     
     $http.get('v1/fotos')
     .success(function(fotos){
@@ -9,6 +11,18 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         console.log(erro);
     });
 
+    $scope.remover = function(foto){
+        $http.delete('v1/fotos/' + foto._id)
+        .success(function() {
+            var indiceFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceFoto,1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso';
+        })
+        .error(function(error){
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+        });
+    };
     /*var promise = $http.get('v1/fotos');
     promise.then(function(retorno){
         $scope.fotos = retorno.data;
--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -10,4 +10,14 @@ angular.module('minhasDiretivas',[])
 
     ddo.templateUrl = 'js/directives/meu-painel.html'
     return ddo;
+})
+.directive('minhaFoto', function() {
+    var ddo = {};
+    ddo.restric = "AE";
+    ddo.scope = {
+        titulo: '@',
+        url:'@'
+    };
+    ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">'
+    return ddo;
 });
